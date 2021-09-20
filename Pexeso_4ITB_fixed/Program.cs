978a94c73b58df14fc990ae0f4c3d19715b9ce08
vratislavino <vratislavino@gmail.com>
2021-09-20 11:11:29 +0200

    fixed version

--- Pexeso_4ITB_fixed/Program.cs
+++ Pexeso_4ITB_fixed/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +9,13 @@ namespace Pexeso
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Hlavní vstupní bod aplikace.
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Game());
+            Application.Run(new Menu());
         }
     }
 }
